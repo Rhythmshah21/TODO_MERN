d7e3f0e0cf94a65868c2e960d600252edcdb7291
Rhythm <rhythmshah21@gmail.com>
2024-05-17 20:06:35 +0530

    server deployment

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import queryString from 'query-string'
 
-const baseUrl = 'http://127.0.0.1:5000/api/v1/'
+const baseUrl = 'todo-mern-jqyt-f4qoyn2rx-rhythmshah21s-projects.vercel.app/api/v1/'
 const getToken = () => localStorage.getItem('token')
 
 const axiosClient = axios.create({
